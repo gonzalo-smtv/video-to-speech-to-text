fe7cdf74d10461cebc5a4fec461da6bb02aa4221
Lopez Gonzalo <lopezgonzalo.jsx@gmail.com>
2024-11-02 22:39:27 +0400

    a

--- app/api/speechToText/route.ts
+++ app/api/speechToText/route.ts
@@ -12,22 +12,22 @@ const openai = new OpenAI({
 // @ts-ignore
 export async function POST(req) {
   try {
+    const body = await req.json();
+    const base64Audio = body.audio;
+    const audio = Buffer.from(base64Audio, "base64");
+    const filePath = "tmp/input.mp3";
+
+    fs.writeFileSync(filePath, audio);
+    const readStream = fs.createReadStream(filePath);
+    const data = await openai.audio.transcriptions.create({
+      file: readStream,
+      model: "whisper-1",
+    });
+    // Remove the file after use
+    fs.unlinkSync(filePath);
+
+    console.log("data: ", data);
     return NextResponse.json({ text: "hola mundo" });
-    // const body = await req.json();
-    // const base64Audio = body.audio;
-    // const audio = Buffer.from(base64Audio, "base64");
-    // const filePath = "tmp/input.mp3";
-
-    // fs.writeFileSync(filePath, audio);
-    // const readStream = fs.createReadStream(filePath);
-    // const data = await openai.audio.transcriptions.create({
-    //   file: readStream,
-    //   model: "whisper-1",
-    // });
-    // // Remove the file after use
-    // fs.unlinkSync(filePath);
-
-    // console.log("data: ", data);
     // return NextResponse.json(data);
   } catch (error) {
     console.error("Error processing audio:", error);
